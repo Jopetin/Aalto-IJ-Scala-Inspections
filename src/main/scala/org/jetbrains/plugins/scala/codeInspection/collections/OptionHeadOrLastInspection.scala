26146d4357acd41b6e5f4745edba04911ef228a3
Joel Toppinen <joel.toppinen@aalto.fi>
2024-05-07 13:41:08 +0300

    Added an inspection implementation for using head or last on Option

---
+++ src/main/scala/org/jetbrains/plugins/scala/codeInspection/collections/OptionHeadOrLastInspection.scala
@@ -0,0 +1,22 @@
+package org.jetbrains.plugins.scala.codeInspection.collections
+
+import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
+import scala.collection.immutable.ArraySeq
+import org.jetbrains.plugins.scala.codeInspection.AaltoInspectionBundle
+
+class OptionHeadOrLastInspection extends OperationOnCollectionInspection {
+  override def possibleSimplificationTypes: ArraySeq[SimplificationType] = ArraySeq(IllegalOptionHeadOrLast)
+}
+
+object IllegalOptionHeadOrLast extends SimplificationType {
+  override def hint: String = AaltoInspectionBundle.message("inspection.illegal.option.head.or.last.usage.description")
+
+
+  override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
+    case `.head`(qual) if isOption(qual) =>
+      Some(replace(expr).withText(qual.getText).highlightFrom(qual))
+    case `.last`(qual) if isOption(qual) =>
+      Some(replace(expr).withText(qual.getText).highlightFrom(qual))
+    case _ => None
+  }
+}
