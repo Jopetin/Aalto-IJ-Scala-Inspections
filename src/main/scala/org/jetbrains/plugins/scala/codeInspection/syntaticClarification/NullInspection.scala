cb1739f78980a515921e5e42f62f8119d5e35dfc
Joel Toppinen <joel.toppinen@aalto.fi>
2024-05-07 13:40:12 +0300

    Added an inspection implementation for null usage

---
+++ src/main/scala/org/jetbrains/plugins/scala/codeInspection/syntaticClarification/NullInspection.scala
@@ -0,0 +1,18 @@
+package org.jetbrains.plugins.scala.codeInspection.syntaticClarification
+
+import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
+import org.jetbrains.plugins.scala.codeInspection.PsiElementVisitorSimple
+import org.jetbrains.plugins.scala.lang.psi.api.base.literals.ScNullLiteral
+import org.jetbrains.plugins.scala.codeInspection.AaltoInspectionBundle
+
+
+class NullInspection extends LocalInspectionTool {
+  override def buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PsiElementVisitorSimple = {
+    case expr: ScNullLiteral =>
+      holder.registerProblem(
+        expr,
+        AaltoInspectionBundle.message("inspection.illegal.null.usage.description")
+      )
+    case _ =>
+  }
+}
