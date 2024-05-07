1e29da09ae07233532125fe39070d7799c7c4f3a
Joel Toppinen <joel.toppinen@aalto.fi>
2024-05-07 13:43:06 +0300

    Updated the Option.get inspection

--- src/main/scala/org/jetbrains/plugins/scala/codeInspection/collections/OptionGetInspection.scala
+++ src/main/scala/org/jetbrains/plugins/scala/codeInspection/collections/OptionGetInspection.scala
@@ -2,13 +2,14 @@ package org.jetbrains.plugins.scala.codeInspection.collections
 
 import org.jetbrains.plugins.scala.lang.psi.api.expr.ScExpression
 import scala.collection.immutable.ArraySeq
+import org.jetbrains.plugins.scala.codeInspection.AaltoInspectionBundle
 
 class OptionGetInspection extends OperationOnCollectionInspection {
   override def possibleSimplificationTypes: ArraySeq[SimplificationType] = ArraySeq(IllegalOptionGet)
 }
 
 object IllegalOptionGet extends SimplificationType {
-  override def hint: String = "Using `get` to fetch a value from Option can cause runtime exceptions"
+  override def hint: String = AaltoInspectionBundle.message("inspection.illegal.option.get.usage.description")
 
 
   override def getSimplification(expr: ScExpression): Option[Simplification] = expr match {
