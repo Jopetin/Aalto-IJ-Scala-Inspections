2eac8ed555fc2f3b641dbdc86de265b73f224efc
Joel Toppinen <jopemies.toppinen@gmail.com>
2024-06-06 11:32:00 +0300

    Fixed cases that involved parentheses, brackets and usage as a parameter

--- src/main/scala/fi/aalto/cs/codeInspection/EffectFreeMethodInspection.scala
+++ src/main/scala/fi/aalto/cs/codeInspection/EffectFreeMethodInspection.scala
@@ -3,13 +3,17 @@ package fi.aalto.cs.codeInspection
 import com.intellij.codeInspection.{LocalInspectionTool, ProblemsHolder}
 import com.intellij.psi.PsiElement
 import org.jetbrains.plugins.scala.codeInspection.PsiElementVisitorSimple
-import org.jetbrains.plugins.scala.lang.psi.api.expr.{
-  ScExpression,
-  ScMethodCall,
-  ScReferenceExpression
-}
+import org.jetbrains.plugins.scala.lang.psi.api.expr.{ScArgumentExprList, ScExpression, ScMethodCall, ScParenthesisedExpr, ScReferenceExpression}
 import org.jetbrains.plugins.scala.lang.psi.api.statements.ScFunctionDefinition
 
+/**
+ * Inspection used to check whether results from methods with given annotation are used in any way
+ *
+ * Essentially when finding a method call to a method with the given annotation, it checks whether it is the last child of its parent in the Psi tree.
+ * Annotation is identified based on its name, which can be configured in the message bundle.
+ * If it is set to a variable or returned by the function, the last line will be the method call itself.
+ * Cases where the method return value is used as a parameter or chained to an another method call are checked through the class of the parent element.
+ */
 class EffectFreeMethodInspection extends LocalInspectionTool:
 
   override def buildVisitor(
@@ -21,14 +25,29 @@ class EffectFreeMethodInspection extends LocalInspectionTool:
         case ref: ScReferenceExpression =>
           ref.resolve() match
             case func: ScFunctionDefinition =>
-              if func.annotations.map(_.getText).contains(AaltoInspectionBundle.message("inspection.effect.free.method.annotation.name"))
-                && !method.getParent.isInstanceOf[ScReferenceExpression]
-                && method.getParent.getLastChild != method
-              then
-                holder.registerProblem(
-                  method,
-                  AaltoInspectionBundle.message("inspection.effect.free.method.description")
-                )
+              if func.annotations.exists(_.textMatches(AaltoInspectionBundle.message("inspection.effect.free.method.annotation.name"))) then
+                var target: PsiElement = method
+                var parent: PsiElement = method.getParent
+                var siblings: Array[PsiElement] = parent.getChildren
+
+                // Unwrap all parenthesis
+                while(parent.isInstanceOf[ScParenthesisedExpr]) do
+                  target = parent
+                  parent = parent.getParent
+                  siblings = parent.getChildren
+                // In case a code block is wrapped in brackets we compare to the last element before the closing bracket
+                if siblings.last.textMatches("}") && siblings.length > 1 then siblings = siblings.dropRight(1)
+                // Check whether the method is the last in its block...
+                if siblings.last != target
+                  // ...or is chained to an another method call...
+                  && !parent.isInstanceOf[ScReferenceExpression]
+                  // ...or is used as a parameter
+                  && !parent.isInstanceOf[ScArgumentExprList]  then
+                  holder.registerProblem(
+                    // Highlights only the method call in question
+                    method.getFirstChild.getLastChild,
+                    AaltoInspectionBundle.message("inspection.effect.free.method.description")
+                  )
             case _ =>
         case _ =>
     case _ =>
